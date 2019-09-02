cd58835520835e816d43ab402cfd6e41b22b130c
Token <41331974+iskenyildiz@users.noreply.github.com>
2019-09-02 17:23:05 +0300

    Create dht11_python3.py

---
+++ dht11_python3.py
@@ -0,0 +1,33 @@
+import grovepi
+import time
+dht_sensor_port=4
+dht_sensor_type=0
+import sys
+import os
+import time
+from datetime import datetime
+import requests
+#now=datetime.now()
+data=open('data.txt','w')
+while True:
+     #   try:
+                [ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
+        #       print("Temp =", temp, "*C Humidity =", hum,"%")
+                print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
+                #print(f"Temp:{temp}*C Humidity:{hum} {datetime.now().strftime('%s')}")
+                time.sleep(1)
+                data.write('temperature,' + 'temp='+ str(temp) + ' ' + 'hum=' +str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
+                #data.write(' ')
+                #data.write('hum=' +str(hum))
+                response = requests.post('http://35.198.129.164:8086/write?db=yeni4', data=f'temperature,temp={int(temp)} hum={hum}')
+                #print(response.status_code)
+                data.write('\n')
+      #  except KeyboardInterrupt as k:
+       #         print("Shutting down.")
+        #        break
+
+
+
+
+
+
