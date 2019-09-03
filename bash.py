c0efbf966e210f51caee6172f4b69e5e4a7fba11
Token <41331974+iskenyildiz@users.noreply.github.com>
2019-09-03 11:57:24 +0300

    Update bash.py

--- bash.py
+++ bash.py
@@ -1,20 +1,24 @@
-
 import grovepi
 import time
+import smtplib
 dht_sensor_port=4
 dht_sensor_type=0
-import sys
-import os
 import time
-from datetime import datetime
-import requests
-#now=datetime.now()
 data=open('data.txt','w')
 [ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
 print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
-time.sleep(1)
-print(type(hum))
 data.write('temperature ' + 'temp='+ str(temp))
 data.write('\n')
 data.write('temperature ' + 'hum='+ str(hum))
+if temp>=25.0: 
+    content = 'Oda sicakligi ' + str(temp) + '*C'
+    mail = smtplib.SMTP("smtp.gmail.com",587) 
+    mail.ehlo()
+    mail.starttls()
+    mail.login('gozenintern@gmail.com','password')
+    mail.sendmail("gozenintern@gmail.com","muhammetkrn19@gmail.com",content)
+
+
+
+
 
