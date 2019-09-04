c2638a6a9bdc84d72856f690058215890393f034
Token <41331974+iskenyildiz@users.noreply.github.com>
2019-09-04 12:00:01 +0300

    Update tempgps.py

--- tempgps.py
+++ tempgps.py
@@ -28,8 +28,8 @@ data.write('temperature,location=Gozen_Holding ' + 'hum='+ str(hum))
 def mail(content):
     mail = smtplib.SMTP("smtp.gmail.com",587) 
     mail.starttls()
-    mail.login('gozenintern@gmail.com','iskenHub123**')
-    mail.sendmail("gozenintern@gmail.com","muhammetkrn19@gmail.com",content)
+    mail.login('yourmail@gmail.com','password')
+    mail.sendmail("yourmail@gmail.com","targetmail@gmail.com",content)
 
 
 if temp>=25.0: 
