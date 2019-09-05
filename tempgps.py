e368b35c77ea3f78d30ca78374967cd5838c1e96
Token <41331974+iskenyildiz@users.noreply.github.com>
2019-09-05 09:17:53 +0300

    Update tempgps.py

--- tempgps.py
+++ tempgps.py
@@ -1,11 +1,14 @@
 import grovepi
 import time
 import smtplib
-dht_sensor_port=4
+dht_sensor_port=4 #DHT11 uses digital ports connected to S
 dht_sensor_type=0
 import time
 import requests
 
+source_mail_address='yourmail@gmail.com'
+target_mail_address='targetmail@gmail.com'
+source_mail_password='passwd'
 
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
 my_ip = ip_request.json()['ip']  
@@ -18,7 +21,9 @@ geo_data = geo_request.json()
 
 org=geo_data["organization"] 
 neworg=org.replace(" ", "_")
-print(org)
+
+
+
 data=open('data.txt','w')
 [ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
 print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
@@ -26,11 +31,12 @@ data.write('temperature,location=' + neworg + ' temp='+ str(temp))
 data.write('\n')
 data.write('temperature,location=' + neworg + ' hum='+ str(hum))
 
+
 def mail(content):
     mail = smtplib.SMTP("smtp.gmail.com",587) 
     mail.starttls()
-    mail.login('yourmail@gmail.com','password')
-    mail.sendmail("yourmail@gmail.com","targetmail@gmail.com",content)
+    mail.login(source_mail_address,source_mail_password)
+    mail.sendmail(source_mail_address,target_mail_address,content)
 
 
 if temp>=25.0: 
