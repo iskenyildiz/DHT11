261b415d4181afc8094d7dc757ee59ecb752a071
Token <41331974+iskenyildiz@users.noreply.github.com>
2019-09-05 09:36:45 +0300

    Update tempgps.py

--- tempgps.py
+++ tempgps.py
@@ -1,15 +1,22 @@
 import grovepi
 import time
 import smtplib
-dht_sensor_port=4 #DHT11 uses digital ports connected to S
-dht_sensor_type=0
+dht_sensor_port=4 # DHT11 uses digital ports connected to I2C ports so change this for your preference. (We used D4 port.)
+dht_sensor_type=0 # 0 for DHT11, 1 for DHT22
 import time
 import requests
 
-source_mail_address='yourmail@gmail.com'
-target_mail_address='targetmail@gmail.com'
-source_mail_password='passwd'
+temp_high_alert=25.0 # Change this depending on how hot or cold the room you think should be for the program to send email alerts.
+hum_high_alert=35  # Change this depending on how hot or cold the room you think should be for the program to send email alerts.
+temp_low_alert=15  # Change this depending on how hot or cold the room you think should be for the program to send email alerts.
+hum_low_alert=15  # Change this depending on how hot or cold the room you think should be for the program to send email alerts.
 
+source_mail_address='yourmail@gmail.com' # Your mail address that you will be sending the mail from.
+target_mail_address='targetmail@gmail.com' # Destination mail address.
+source_mail_password='passwd' # password of your mail address for logging in.
+
+
+# This is taken from https://www.freecodecamp.org/forum/t/make-a-script-read-gps-geolocation/241607/2
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
 my_ip = ip_request.json()['ip']  
 #print(my_ip)
@@ -19,35 +26,49 @@ geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
 geo_request = requests.get(geo_request_url)
 geo_data = geo_request.json()
 
-org=geo_data["organization"] 
-neworg=org.replace(" ", "_")
+geojs_data=geo_data["organization"] # we store the geojs data in a variable called geojs_data.
+
+# instead of organization you can use either of below whichever you need for that instance.
+# "area_code"
+# "continent_code"
+# "country"
+# "country_code"
+# "country_code3"
+# "ip"
+# "latitude"
+# "longitude"
+# "organization"
+# "timezone"
+
+underscored_geojs_data=geojs_data.replace(" ", "_") # since InfluxDB line protocol doesn't support spaces other than the space between tag value and field value we put underscores for the spaces found in tag value.
 
 
+data=open('data.txt','w') # This is the file we create that will be sent to the influxDB database for storage. We will fill this with information below.
+[ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type) # Grovepi sensor function to store temperature and humidity.
+print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime()))) # prints the data and time.
 
-data=open('data.txt','w')
-[ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
-print ('Temp: '+ str(temp) + '*C' + '\tHumidity:' + ' %'+ str(hum) + ' ' + str(time.strftime("%s",time.gmtime())))
-data.write('temperature,location=' + neworg + ' temp='+ str(temp))
+# Write the data taken into data.txt below. The format is written as it is because the line protocol is strict about the typo.
+data.write('temperature,location=' + underscored_geojs_data + ' temp='+ str(temp))
 data.write('\n')
-data.write('temperature,location=' + neworg + ' hum='+ str(hum))
+data.write('temperature,location=' + underscored_geojs_data + ' hum='+ str(hum))
 
-
-def mail(content):
+# Sending the mails an email.
+def mail(content): # content is defined in the if statements below, which is the message to be sent in the email.
     mail = smtplib.SMTP("smtp.gmail.com",587) 
     mail.starttls()
     mail.login(source_mail_address,source_mail_password)
     mail.sendmail(source_mail_address,target_mail_address,content)
 
-
-if temp>=25.0: 
-    content = 'Oda sicakligi ' + str(temp) + '*C' + ' Sicaklik yuksek!'
+# Send mail depending on the room temperature and humidity.
+if temp>=temp_high_alert: 
+    content = 'Room temperature ' + str(temp) + '*C' + ', Temperature is too high!'
     mail(content)
-elif temp<=15:
-    content = 'Oda sicakligi ' + str(temp) + '*C' + ' Sicakligi artirmaniz onerilir.'
+elif temp<=temp_low_alert:
+    content = 'Room temperature ' + str(temp) + '*C' + ', You are advised to increase the temperature.'
     mail(content)
-elif hum >=35:
-    content = 'Odadaki nem orani  %' +  str(hum)  + ' Nem orani cok yuksek!'
+elif hum >=hum_high_alert:
+    content = 'Humidity inside the room  %' +  str(hum)  + ', Humidity is too high!'
     mail(content)
-elif hum <=15:
-    content = 'Odadaki nem orani  %' +  str(hum)  + ' Nem orani cok dusuk!'
+elif hum <=hum_low_alert:
+    content = 'Humidity inside the room  %' +  str(hum)  + ', Humidity is too low!'
     mail(content)
